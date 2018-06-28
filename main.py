bd57f56026500677454e5353cffd07ab65e9e26f
Rodrigo Dias <rodrigo.drds@gmail.com>
2018-06-28 10:31:52 -0300

    try to use tkinter

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# import opsutils  
+import os
+from tkinter import *
+from tkinter import filedialog
+
 
 if __name__ == "__main__":
     version = "1.0.1"
@@ -5,3 +10,14 @@ if __name__ == "__main__":
     name = input("Whats your name: ")
     print(f"Hello, {name}")
     input("pause...")
+    pasta = os.path.expanduser("~")
+
+    root = Tk()
+    root.filename =  filedialog.askopenfilename(
+        initialdir=pasta, 
+        title="Select file", 
+        filetypes = (("jpeg files","*.jpg"),("all files","*.*"))
+    )
+    filename = root.filename
+    root.destroy()
+    print(filename)
